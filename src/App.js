3f8d264329f6bf90d64fb7cafa9074b81ea95cfa
john-smilga <johnsmilga@gmail.com>
2020-10-19 22:06:50 -0700

    removed loading setup

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import {
   FaLock,
 } from 'react-icons/fa'
 const url = 'https://randomuser.me/api/'
-
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
   const [loading, setLoading] = useState(true)
   const [person, setPerson] = useState(null)
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('random person')
   const getPerson = async () => {
     setLoading(true)
     const response = await fetch(url)
@@ -56,64 +56,58 @@ function App() {
     <main>
       <div className='block bcg-black'></div>
       <div className='block'>
-        {loading ? (
-          <div className='container'>
-            <h2 className='loading'>Loading...</h2>
-          </div>
-        ) : (
-          <div className='container'>
-            <img src={person.image} alt='random user' className='user-img' />
-            <p className='user-title'>My name is</p>
-            <p className='user-value'>{value}</p>
-            <div className='values-list'>
-              <button
-                className='icon'
-                data-label='name'
-                onMouseOver={handleValue}
-              >
-                <FaUser />
-              </button>
-              <button
-                className='icon'
-                data-label='email'
-                onMouseOver={handleValue}
-              >
-                <FaEnvelopeOpen />
-              </button>
-              <button
-                className='icon'
-                data-label='age'
-                onMouseOver={handleValue}
-              >
-                <FaCalendarTimes />
-              </button>
-              <button
-                className='icon'
-                data-label='street'
-                onMouseOver={handleValue}
-              >
-                <FaMap />
-              </button>
-              <button
-                className='icon'
-                data-label='phone'
-                onMouseOver={handleValue}
-              >
-                <FaPhone />
-              </button>
-              <button
-                className='icon'
-                data-label='password'
-                onMouseOver={handleValue}
-              >
-                <FaLock />
-              </button>
-            </div>
-            <button className='btn' type='button' onClick={getPerson}>
-              random user
+        <div className='container'>
+          <img
+            src={(person && person.image) || defaultImage}
+            alt='random user'
+            className='user-img'
+          />
+          <p className='user-title'>My name is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            <button
+              className='icon'
+              data-label='name'
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className='icon'
+              data-label='email'
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className='icon' data-label='age' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className='icon'
+              data-label='street'
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className='icon'
+              data-label='phone'
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className='icon'
+              data-label='password'
+              onMouseOver={handleValue}
+            >
+              <FaLock />
             </button>
           </div>
-        )}
+          <button className='btn' type='button' onClick={getPerson}>
+            {loading ? 'loading...' : 'random user'}
+          </button>
+        </div>
       </div>
     </main>
   )
